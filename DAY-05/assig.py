eb352acbda2afe1ea9a3f0ff31bf2b4583ac55bb
Mupparaju19 <143019131+Mupparaju19@users.noreply.github.com>
2025-01-01 17:51:36 -0500

    Update assig.py

--- DAY-05/assig.py
+++ DAY-05/assig.py
@@ -1,1 +1,48 @@
+#Task 1: Arithmetic Operators
+#Create two variables a and b with numeric values.
+#Calculate the sum, difference, product, and quotient of a and b.
+#Print the results.
+a=5
+b=6
+print(a-b)
+print(a+b)
+print(a*b)
+print(a/b)
+
+#Task 2: Comparison Operators
+#Compare the values of a and b using the following comparison operators: <, >, <=, >=, ==, and !=.
+#Print the results of each comparison.
+a=6
+b=5
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+#Task 3: Logical Operators
+#Create two boolean variables, x and y.
+#Use logical operators (and, or, not) to perform various logical operations on x and y.
+#Print the results.
+
+x=True
+y=False 
+print(x and y)      
+print(x or y)         
+print(not x)           
+print(not y)           
+
+#Task 4: Assignment Operators
+#Create a variable total and initialize it to 10.
+#Use assignment operators (+=, -=, *=, /=) to update the value of total.
+#Print the final value of total.
+
+total = 10
+total+= 10 
+total-= 10
+total*= 10
+total/= 10
+print(total)
+
 
